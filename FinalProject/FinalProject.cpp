3c2c37170aa9db6f372cec912c734e489670b847
TheRealAt0th3t <NerdGasm4049!Pokemon4049!>
2021-12-01 16:18:01 -0700

    added header information

--- FinalProject/FinalProject.cpp
+++ FinalProject/FinalProject.cpp
@@ -1,4 +1,13 @@
-
+/**
+ * @file FinalProject.cpp
+ * @author Amara Tariq
+ * @brief GIMM 280 - Parking Garage Assistant
+ * @version 0.1
+ * @date 2021-12-01
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 #include <FastLED.h> //to help control individual LEDs
 #include <QuickStats.h> //to help manage stats for echo and trig
 
